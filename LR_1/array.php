1ae15d1b3e35f02d8dc57b0885572b40eb8ef480
AlinaLapushinskaya <48092481+AlinaLapushinskaya@users.noreply.github.com>
2019-09-13 14:58:09 +0300

    Create array.php

---
+++ LR_1/array.php
@@ -0,0 +1,12 @@
+<?php
+$keys = ['sky', 'grass', 'orange'];
+$values = ['blue', 'green', 'orange'];
+ 
+$array = array_combine($keys, $values);
+print_r($array);
+$arrays = [[1, 2], [3, 4], [5, 6]];
+ 
+foreach ($arrays as list($a, $b)) {
+    $c = $a + $b;
+    echo($c . ', '); 
+}
